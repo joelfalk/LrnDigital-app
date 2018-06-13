a8ea9c5b699109764db7e30a33fdae995443ae6e
wukebin <dic14kng@student.lu.se>
2018-06-13 14:52:49 +0200

    testcommit1

---
+++ app/src/main/java/com/example/kev/lrndigitalapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kev.lrndigitalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+/* heeeeej */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
