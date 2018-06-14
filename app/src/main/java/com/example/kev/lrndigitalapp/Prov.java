d7503ab8e7ee00fa3c9d77b2b418aebf636dcb7e
wukebin <dic14kng@student.lu.se>
2018-06-14 14:50:10 +0200

    try on virtual device

---
+++ app/src/main/java/com/example/kev/lrndigitalapp/Prov.java
@@ -0,0 +1,13 @@
+package com.example.kev.lrndigitalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Prov extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prov);
+    }
+}
